a06c9fd21fce22c151fe0a9261f3938b34776692
raihannizar24 <raihannizar24@gmail.com>
2024-01-29 23:22:14 +0700

    feat(add note): make adding notes to note list works

--- src/components/NoteInput.jsx
+++ src/components/NoteInput.jsx
@@ -1,7 +1,39 @@
+"use client"
+
+import { useState } from "react"
+import { useRouter } from "next/navigation";
+
 export const NoteInput = () => {
-  return (
-    <div className="grow bg-stone-300 p-4">
-      NoteInput
+  const router = useRouter();
+  const [noteTitle, setNoteTitle] = useState("");
+  const [noteContent, setNoteContent] = useState("");
+
+  const noteData = [{ "title": noteTitle, "content": noteContent }]
+
+  async function postNoteData() {
+    const res = await fetch("https://v1.appbackend.io/v1/rows/rz4KwC0FcIUZ", {
+      method: "POST",
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(noteData)
+    });
+    const resJson = await res.json();
+    console.log(resJson);
+
+    // refresh agar re-fetch setelah post data
+    router.refresh();
+
+    // kosongkan text area
+    setNoteTitle("");
+    setNoteContent("");
+  }
+
+return (
+  <div className="grow flex flex-col bg-stone-300 p-20 gap-y-2 items-end">
+    <div className="grow flex flex-col gap-y-2 w-full">
+      <textarea className="input-title h-16 bg-stone-300 rounded-md text-3xl font-bold resize-none outline-none" placeholder="Masukan judul catatan di sini..." value={noteTitle} onChange={(e) => setNoteTitle(e.target.value)}></textarea>
+      <textarea className="input-content grow bg-stone-300 rounded-md resize-none outline-none" placeholder="Masukan teks catatan di sini..." value={noteContent} onChange={(e) => setNoteContent(e.target.value)}></textarea>
     </div>
-  )
+    <button className="bg-orange-400 p-2 rounded-md" onClick={postNoteData}>Simpan</button>
+  </div>
+)
 }
