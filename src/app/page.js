2c8efbb58c2296193a485452976a681ca2996af5
raihannizar24 <raihannizar24@gmail.com>
2024-01-29 22:21:48 +0700

    feat(list): render note list from API

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,18 @@
 import { NoteInput } from "@/components/NoteInput";
 import { NoteList } from "@/components/NoteList";
-import Image from "next/image";
 
-export default function Home() {
+async function getAllNotes() {
+  const res = await fetch("https://v1.appbackend.io/v1/rows/rz4KwC0FcIUZ");
+  const allNotes = await res.json();
+  return allNotes;
+}
+
+export default async function Home() {
+  const { data } = await getAllNotes();
+
   return (
     <div className="grow flex flex-row gap-x-2">
-      <NoteList />
+      <NoteList allNotes={data}/>
       <NoteInput />
     </div>
   );
--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -1,7 +1,12 @@
-export const NoteList = () => {
+export const NoteList = ({ allNotes }) => {
   return (
-    <div className="bg-stone-400 p-4">
-      NoteList
+    <div className="bg-stone-400 p-4 flex flex-col gap-y-2">
+      {allNotes.map(note => (
+        <div key={note._id} className="bg-stone-300 p-4">
+          <h2 className="font-bold mb-2">{note.title}</h2>
+          <p>{note.content}</p>
+        </div>
+      ))}
     </div>
   )
 }
