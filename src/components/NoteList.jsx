07487a92bc3a0a2cb459b679883230dbf6be6808
raihannizar24 <raihannizar24@gmail.com>
2024-01-30 15:35:37 +0700

    fix(note list): truncate ellipsis for overflowing text

--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -6,12 +6,12 @@ export const NoteList = ({ allNotes, setSelectedNoteId, setEditMode }) => {
   }
 
   return (
-    <div className="bg-stone-400 p-4 w-40 flex flex-col gap-y-2">
+    <div className="bg-stone-400 p-4 w-52 flex flex-col gap-y-2">
       {allNotes.length > 0
         ? allNotes.map(note => (
           <div key={note._id} className="bg-stone-300 p-4 hover:outline hover:bg-stone-400 cursor-pointer" onClick={() => handleNoteClick(note)}>
-            <h2 className="font-bold mb-2">{note.title}</h2>
-            <p>{note.content}</p>
+            <h2 className="font-bold mb-2 truncate">{note.title}</h2>
+            <p className="truncate">{note.content}</p>
           </div>
         ))
         : <div className="text-stone-600 italic">tidak ada notes!</div>}
