4709299937ac641d032b4c6c0ad07ca3d32aba5b
raihannizar24 <raihannizar24@gmail.com>
2024-01-30 15:28:32 +0700

    fix: if title / content empty, user will be able to submit

--- src/components/NoteInput.jsx
+++ src/components/NoteInput.jsx
@@ -8,6 +8,7 @@ export const NoteInput = ({ selectedNote, editMode, setEditMode }) => {
   const router = useRouter();
   const [noteTitle, setNoteTitle] = useState("");
   const [noteContent, setNoteContent] = useState("");
+  const [canSave, setCanSave] = useState(false);
 
   useEffect(() => {
     if (editMode) {
@@ -15,6 +16,25 @@ export const NoteInput = ({ selectedNote, editMode, setEditMode }) => {
       setNoteContent(selectedNote.content);
     }
   }, [editMode, selectedNote]);
+
+  // jika salah satu input kosong, tidak bisa submit
+  useEffect(() => {
+    if (noteTitle && noteContent) {
+      setCanSave(true)
+    } else {
+      setCanSave(false)
+    }
+  }, [noteTitle, noteContent])
+
+  function handleNoteChange(e, type) {
+    if (type === "title") {
+      setNoteTitle(e.target.value);
+    }
+    
+    if (type === "content") {
+      setNoteContent(e.target.value);
+    }
+  }
 
   function createNewNote() {
     // kembalikan ke mode create
@@ -50,12 +70,12 @@ export const NoteInput = ({ selectedNote, editMode, setEditMode }) => {
       console.error(resJson);
     }
 
-    // refresh agar re-fetch setelah data berubah
-    router.refresh();
-
     // kosongkan text area
     setNoteTitle("");
     setNoteContent("");
+
+    // refresh agar re-fetch setelah data berubah
+    router.refresh();
   }
 
   async function deleteNote() {
@@ -72,26 +92,26 @@ export const NoteInput = ({ selectedNote, editMode, setEditMode }) => {
       console.error(resJson);
     }
 
-    // refresh agar re-fetch setelah data berubah
-    router.refresh();
-
     // kosongkan text area
     setNoteTitle("");
     setNoteContent("");
 
     // reset ke create mode
     createNewNote();
+
+    // refresh agar re-fetch setelah data berubah
+    router.refresh();
   }
 
   return (
     <div className="grow flex flex-col bg-stone-300 p-20 gap-y-2 items-end">
       <div className="grow flex flex-col gap-y-2 w-full">
-        <textarea className="input-title h-16 bg-stone-300 rounded-md text-3xl font-bold resize-none outline-none" placeholder="Masukan judul catatan di sini..." value={noteTitle} onChange={(e) => setNoteTitle(e.target.value)}></textarea>
-        <textarea className="input-content grow bg-stone-300 rounded-md resize-none outline-none" placeholder="Masukan teks catatan di sini..." value={noteContent} onChange={(e) => setNoteContent(e.target.value)}></textarea>
+        <textarea className="input-title h-16 bg-stone-300 rounded-md text-3xl font-bold resize-none outline-none" placeholder="Masukan judul notes di sini..." value={noteTitle} onInput={(e) => handleNoteChange(e, "title")}></textarea>
+        <textarea className="input-content grow bg-stone-300 rounded-md resize-none outline-none" placeholder="Masukan teks notes di sini..." value={noteContent} onInput={(e) => handleNoteChange(e, "content")}></textarea>
       </div>
       <div className="flex flex-row gap-x-2">
         <button className="bg-stone-400 p-2 rounded-md" onClick={createNewNote}>Buat Note Baru +</button>
-        <button className="bg-orange-400 p-2 rounded-md" onClick={saveNoteData}>Simpan</button>
+        <button className="bg-orange-400 p-2 rounded-md disabled:bg-orange-300 disabled:text-slate-700" disabled={!canSave} onClick={saveNoteData}>Simpan</button>
         {editMode
           ? <button className="bg-rose-600 text-white p-2 rounded-md" onClick={deleteNote}>Delete Note -</button>
           : null
